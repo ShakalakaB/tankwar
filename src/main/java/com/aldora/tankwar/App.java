2ede829c5ebb38950f7e513677a957b8f7ffea58
Yunjie <luoyunjie@linghit.com>
2021-03-06 16:01:51 +0800

    add frame

--- src/main/java/com/aldora/tankwar/App.java
+++ src/main/java/com/aldora/tankwar/App.java
@@ -1,13 +1,29 @@
 package com.aldora.tankwar;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JComponent {
+    public App() {
+        this.setPreferredSize(new Dimension(800, 600));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),
+                400, 100, null);
+        super.paintComponent(g);
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("tank war");
+        frame.setTitle("tank war");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new App());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
