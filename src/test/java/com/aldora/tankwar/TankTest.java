9986a511f9742f144fe9bf11caca60178b73dbaf
Yunjie <luoyunjie@linghit.com>
2021-03-12 01:25:49 +0800

    edit getImage

---
+++ src/test/java/com/aldora/tankwar/TankTest.java
@@ -0,0 +1,12 @@
+package com.aldora.tankwar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TankTest {
+
+    @Test
+    void getImage() {
+    }
+}
