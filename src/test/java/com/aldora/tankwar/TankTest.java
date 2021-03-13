84ef9ed085bb3bc0c36461d274fd529c123a98d2
Yunjie <luoyunjie@linghit.com>
2021-03-13 16:45:20 +0800

    add TankTest for direction image

--- src/test/java/com/aldora/tankwar/TankTest.java
+++ src/test/java/com/aldora/tankwar/TankTest.java
@@ -1,12 +1,19 @@
 package com.aldora.tankwar;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TankTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void getImage() {
+        for (Direction direction : Direction.values()) {
+            Tank tank = new Tank(0, 0, direction, false);
+
+            assertTrue(tank.getImage().getWidth(null) > 0, direction + " failed");
+
+            Tank enemyTank = new Tank(0, 0, direction, true);
+
+            assertTrue(enemyTank.getImage().getWidth(null) > 0);
+        }
     }
 }
